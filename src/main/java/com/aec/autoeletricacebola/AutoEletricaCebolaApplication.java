a1fa9237870a0d88cadd8b322f0536d193e23f96
gamartins <gabriel.550@hotmail.com>
2021-09-22 20:58:21 -0300

    Configuração: Upload do projeto

---
+++ src/main/java/com/aec/autoeletricacebola/AutoEletricaCebolaApplication.java
@@ -0,0 +1,13 @@
+package com.aec.autoeletricacebola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEletricaCebolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoEletricaCebolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aec/autoeletricacebola/AutoEletricaCebolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aec.autoeletricacebola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEletricaCebolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

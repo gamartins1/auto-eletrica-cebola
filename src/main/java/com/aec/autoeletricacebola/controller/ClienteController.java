b354770ed169f61f3e72d26d5d8b19c0446c2f5f
gamartins <gabriel.550@hotmail.com>
2021-10-10 20:53:26 -0300

    Implementação: Inicio da criação da tela de cadastro do cliente

--- src/main/java/com/aec/autoeletricacebola/controller/ClienteController.java
+++ src/main/java/com/aec/autoeletricacebola/controller/ClienteController.java
@@ -1,4 +1,21 @@
 package com.aec.autoeletricacebola.controller;
 
+import com.aec.autoeletricacebola.model.Cliente;
+import com.aec.autoeletricacebola.model.TelefoneCliente;
+import com.aec.autoeletricacebola.model.Veiculo;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 public class ClienteController {
+
+    @RequestMapping(value = "cliente/saveWithCarAndPhone", method = RequestMethod.POST)
+    public String saveClientWithCarAndPhone(Cliente cliente, Veiculo veiculo, TelefoneCliente telefoneCliente, BindingResult result, RedirectAttributes attributes) {
+
+
+        return "redirect:/menu";
+    }
 }

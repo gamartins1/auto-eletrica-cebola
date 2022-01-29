790d3a95342ffd630061f48864f88471ded83cf0
gamartins <gabriel.550@hotmail.com>
2022-01-29 20:53:58 -0300

    Refatoração: Ordenação de constantes por ordem alfabética

--- src/main/java/com/aec/autoeletricacebola/utils/ModelAttributeKeys.java
+++ src/main/java/com/aec/autoeletricacebola/utils/ModelAttributeKeys.java
@@ -2,67 +2,88 @@ package com.aec.autoeletricacebola.utils;
 
 public class ModelAttributeKeys {
 
-    public static final String MODELO_VEICULO = "modeloVeiculo";
-    public static final String PLACA_VEICULO = "placaVeiculo";
-    public static final String OBSERVACOES_VEICULO = "observacoesVeiculo";
-    public static final String ID_CLIENTE = "idCliente";
-    public static final String ID_VEICULO = "idVeiculo";
-    public static final String ID_SERVICO = "idServico";
-    public static final String ID_MECANICO = "idMecanico";
-    public static final String IS_ATIVO = "isAtivo";
-    public static final String DESCRICAO_SERVICOS = "descricaoServicos";
-    public static final String CLIENTE = "cliente";
-    public static final String QUANTIDADE_CLIENTES = "quantidadeClientes";
-    public static final String CLIENTES_LISTA = "clientesLista";
-    public static final String SERVICOS_LISTA = "servicosLista";
-    public static final String VEICULOS_LISTA = "veiculosLista";
-    public static final String MECANICOS_LISTA = "mecanicosLista";
 
-    public static final String MAOS_DE_OBRA_SERVICO = "maosDeObraServico";
-
-    public static final String MENSAGEM = "mensagem";
-
-    public static final String SERVICOS = "servicos";
-    public static final String QUANTIDADE_SERVICOS = "quantidadeServicos";
-
-    public static final String VEICULOS = "veiculos";
-
-    public static final String VALOR_FINAL_SERVICO = "valorFinalServico";
-
-    public static final String PECAS_SERVICO = "pecasServico";
-
-    public static final String QUANTIDADE_VEICULOS = "quantidadeVeiculos";
-
-    public static final String NOTA_SERVICO = "notaServico";
-
-    public static final String NOME_CLIENTE = "nomeCliente";
-
+    /** B **/
     public static final String BAIRRO_ENDERECO_CLIENTE = "bairroEnderecoCliente";
 
+    /** C **/
     public static final String CEP_ENDERECO_CLIENTE = "cepEnderecoCliente";
+    public static final String CIDADE_ENDERECO_CLIENTE = "cidadeEnderecoCliente";
+    public static final String CLIENTE = "cliente";
+    public static final String CLIENTES_LISTA = "clientesLista";
 
-    public static final String RUA_ENDERECO_CLIENTE = "ruaEnderecoCliente";
+    /** D **/
+    public static final String DESCRICAO_PECA = "descricaoPeca";
+    public static final String DESCRICAO_SERVICOS = "descricaoServicos";
 
+    /** E **/
+
+    /** F **/
+
+    /** G **/
+
+    /** H **/
+
+    /** I **/
+    public static final String ID_CLIENTE = "idCliente";
+    public static final String ID_MECANICO = "idMecanico";
+    public static final String ID_SERVICO = "idServico";
+    public static final String ID_VEICULO = "idVeiculo";
+    public static final String IS_ATIVO = "isAtivo";
+
+    /** J **/
+    /** K **/
+    /** L **/
+
+    /** M **/
+    public static final String MAOS_DE_OBRA_SERVICO = "maosDeObraServico";
+    public static final String MECANICOS = "mecanicos";
+    public static final String MECANICOS_LISTA = "mecanicosLista";
+    public static final String MENSAGEM = "mensagem";
+    public static final String MODELO_VEICULO = "modeloVeiculo";
+
+    /** N **/
+    public static final String NOME_CLIENTE = "nomeCliente";
+    public static final String NOME_MECANICO = "nomeMecanico";
+    public static final String NOTA_SERVICO = "notaServico";
     public static final String NUMERO_ENDERECO_CLIENTE = "numeroEnderecoCliente";
 
-    public static final String CIDADE_ENDERECO_CLIENTE = "cidadeEnderecoCliente";
+    /** O **/
+    public static final String OBSERVACOES_VEICULO = "observacoesVeiculo";
 
-    public static final String TELEFONES_CLIENTE = "telefonesCliente";
-
-    public static final String NOME_MECANICO = "nomeMecanico";
-
-    public static final String MECANICOS = "mecanicos";
-
-    public static final String QUANTIDADE_MECANICOS = "quantidadeMecanicos";
-
-    public static final String DESCRICAO_PECA = "descricaoPeca";
-
-    public static final String QUANTIDADE_PECA = "quantidadePeca";
-
-    public static final String TEMPO_GARANTIA = "tempoGarantia";
-
+    /** P **/
+    public static final String PLACA_VEICULO = "placaVeiculo";
+    public static final String PECAS_SERVICO = "pecasServico";
     public static final String PRECO_COMPRA = "precoCompra";
-
     public static final String PRECO_VENDA = "precoVenda";
 
+    /** Q **/
+    public static final String QUANTIDADE_CLIENTES = "quantidadeClientes";
+    public static final String QUANTIDADE_MECANICOS = "quantidadeMecanicos";
+    public static final String QUANTIDADE_PECA = "quantidadePeca";
+    public static final String QUANTIDADE_SERVICOS = "quantidadeServicos";
+    public static final String QUANTIDADE_VEICULOS = "quantidadeVeiculos";
+
+    /** R **/
+    public static final String RUA_ENDERECO_CLIENTE = "ruaEnderecoCliente";
+
+    /** S **/
+    public static final String SERVICOS = "servicos";
+    public static final String SERVICOS_LISTA = "servicosLista";
+
+    /** T **/
+    public static final String TELEFONES_CLIENTE = "telefonesCliente";
+    public static final String TEMPO_GARANTIA = "tempoGarantia";
+
+    /** U **/
+
+    /** V **/
+    public static final String VALOR_FINAL_SERVICO = "valorFinalServico";
+    public static final String VEICULOS = "veiculos";
+    public static final String VEICULOS_LISTA = "veiculosLista";
+
+    /** W **/
+    /** X **/
+    /** Y **/
+    /** Z **/
 }
